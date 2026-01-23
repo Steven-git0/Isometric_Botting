44f17ee5363e8a12cd04e125d191804e8015cc58
steven <steven.ydx12m@gmail.com>
2026-01-23 06:38:15 +0000

    linux support

--- Scripts_cooking/hosidius_cooking.py
+++ Scripts_cooking/hosidius_cooking.py
@@ -1,10 +1,15 @@
+import sys
+import os
+
+# Add the parent directory (Isometric_Botting) to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from core_scripts.screen_scraping import *
-from core_scripts.movements import *
-import pygetwindow as gw
+from core_scripts.movements import *                                                                                                                                                    
 import time
 import random
 from PIL import Image
 import imagehash
+import pyautogui
 
 #make sure last item in inventory is the item to be cooked
 #item must be visible in the bank on the first page
@@ -14,17 +19,9 @@ import imagehash
 
 window_title = "RuneLite - litlGenocide"
 
-window = gw.getWindowsWithTitle(window_title)[0]
-if not window:
-    print(f"No window found with title '{window_title}'")
-    exit()
-rx, ry, width, height = window.left, window.top, window.width, window.height
-
 mouse_movements = mouse_movements(window_title)
 screenscrape = screenscrape(window_title)
-
 cook_item = screenscrape.last_inventory()
-
 counter = 0 
 deposit_x, deposit_y = 0, 0
 raw_x, raw_y = 0, 0
@@ -40,28 +37,31 @@ while counter < 280:
     time.sleep(random.uniform(68, 75))
 
     #random break
-    # if random.uniform(1.00, 10.00) > 9.2:
-    #     time.sleep(random.uniform(60, 600))
+    if random.uniform(1.00, 10.00) > 9.2:
+        time.sleep(random.uniform(60, 600))
 
     #go back to bank chest
     mouse_movements.move_mouse(.43679, .70152, 4)
     mouse_movements.perform_click()
+    if counter < 1:
+        mouse_movements.move_mouse(.65, .65, 20)
     time.sleep(random.uniform(3, 3.35))
 
     #find the deposit button and raw food for the first time
     if counter < 1:
         deposit_x, deposit_y = screenscrape.hash_detection('7efecbc1c9c2fe00', 40, 40)
-        raw_x, raw_y = screenscrape.img_detection(cook_item, threshold = .90)[0]
     
     #deposit inventory
     mouse_movements.move_mouse(deposit_x, deposit_y, 4)
     mouse_movements.perform_click()
     time.sleep(random.uniform(.5, 1.2))
 
+    if counter < 1:
+        raw_x, raw_y = screenscrape.img_detection(cook_item, threshold = .90)[0]
     #withdraw raw food
     mouse_movements.move_mouse(raw_x, raw_y, 4)
     mouse_movements.perform_click()
-    time.sleep(random.uniform(.3, 1.1))
+    time.sleep(random.uniform(.5, 2.1))
     
     #leave bank
     pyautogui.press('esc')
---
+++ core_scripts/movements.py
@@ -0,0 +1,156 @@
+#Copper Mining Varrock Eastside, orientation North, 1400x 1000, zoom out max, then zoom in 3 mousewheels
+import pyautogui 
+import time
+import numpy as np
+import random
+from core_scripts.screen_scraping import *
+from humancursor import SystemCursor
+import pywinctl as gw
+cursor = SystemCursor()
+
+#Time to mine, adjust depending on your pickaxe and mining level (lower if better pick axe)
+
+class mouse_movements():
+
+    def __init__(self, window_title): 
+        windows = gw.getWindowsWithTitle(window_title)
+        window = windows[0]
+        self.rx = window.left
+        self.ry = window.top
+        self.width = window.width
+        self.height = window.height
+    
+    def perform_click(self, m = 'l'):
+        if m == 'r':
+            print('clicking')
+            pyautogui.mouseDown(pyautogui.position(), button='right')
+            time.sleep(random.uniform(.10, .20))
+            pyautogui.mouseUp(pyautogui.position(), button='right')
+        else:
+            print('clicking')
+            pyautogui.mouseDown(pyautogui.position())
+            time.sleep(random.uniform(.10, .20))
+            pyautogui.mouseUp(pyautogui.position())
+            
+
+    def move_mouse(self, end_x, end_y, variance = 2):
+        
+        if end_x <= 1 and end_y <= 1:
+            end_x = self.rx + (self.width*end_x) + random.uniform(-variance, variance)
+            end_y = self.ry + (self.height*end_y) + random.uniform(-variance, variance)
+        else:
+            end_x += random.uniform(-variance, variance) 
+            end_y += random.uniform(-variance, variance)
+        attempts = 0
+        while attempts < 5:
+            try:
+                print('move to ', end_x, end_y)
+                cursor.move_to([end_x, end_y], duration = 0.01)
+                break
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print("1 attempt failed, rounding to whole")
+                end_x = round(end_x)
+                end_y = round(end_y)
+
+        if attempts == 5:
+             print("Max retries reached, operation failed.")
+
+        #cursor.click_on([end_x, end_y])
+
+    def click_image(self, img_name, element = 0, threshold = .90):
+        image = Image.open(img_name)
+        width, height = image.size[0] // 2, image.size[1] // 2
+        try:
+            x,y = self.screenscrape.img_detection(img_name, threshold)[element]
+            mouse_movements.move_mouse(x+width, y+height, 2)
+            mouse_movements.perform_click()
+        except:
+            return False
+    
+    def move_contours(self, x_axis = True, right2left = False, color = [50,0,73]):
+        #color of teal markers in runelite is [0,253,242]
+        #right to left is descending order as coords is 0, 0 at the top left
+        #find the teal contours
+        markers = self.screenscrape.find_contours(target_color = color)
+        #filter out the minimap contours
+        filtered_markers = [(x,y) for x,y in markers if not (self.width-215 <= x <= self.width 
+                                                             and 0 <= y <= 200)]
+        #Sort by x coordinates, else y coordinates
+        if x_axis == True:
+            filtered_markers = sorted(filtered_markers, key= lambda x: x[0], reverse = right2left)
+        else:
+            filtered_markers = sorted(filtered_markers, key= lambda x: x[1], reverse = right2left)
+
+        for x,y in filtered_markers:
+            print(x, y)
+        return filtered_markers
+    
+    def relative_move(self, direction = None, offset = 60):
+
+        center_x, center_y = self.get_center()
+        filtered_markers = self.move_contours()
+        coordinates = filtered_markers
+        #filter out a direction you dont want the player to go
+        if direction == 'up':
+            #remember top left is 0, 0 higher y is the lower it is. -80 to shift the cenmtre point as to not include center 
+            filtered_markers = [(x,y) for x, y in coordinates if y <= center_y-offset]
+            print("moving up")
+        elif direction == 'down':
+            filtered_markers = [(x,y) for x, y in coordinates if y >= center_y+offset]
+            print("moving down")
+        elif direction == 'right':
+            filtered_markers = [(x,y) for x, y in coordinates if x >= center_x+offset]
+            print("moving right")
+        elif direction == 'left':
+            filtered_markers = [(x,y) for x, y in coordinates if x <= center_x-offset]
+            print("moving left")
+        else:
+            print('no direction specified, moving to closest marker')
+
+        sum_list = []
+        #for every marker it finds it deducts the centre coords to find the lowest value
+        #the lowest value will be the closest coordinate
+        for x,y in filtered_markers:
+            #the center + offset allows you change the effective center if you want another coord
+            sum_list.append(abs(x - (center_x)) + abs(y- (center_y)))
+
+        #get the index of the lowest value in the sum list
+        min_index = sum_list.index(min(sum_list))
+        print(min_index)
+
+        return filtered_markers[min_index]
+
+    def get_center(self):
+        # Calculate the center of the window
+        #-17 is specific the the runelite client to compensate for the bar on the right.
+        center_x = (self.width // 2) - 17
+        center_y = (self.height // 2) + 20
+
+        print(f"center at ({center_x}, {center_y})")
+
+        return center_x, center_y
+    
+    def get_minimap(self):
+
+        window_top = self.ry
+        window_width = self.width
+
+        #osrs minimap size does not change and is always x-260 and y+ 230 fromt he top right corner
+        return window_width - 260, window_width, window_top, window_top + 230
+    
+    def login(self):
+        
+        mouse_movements.move_mouse(self, self.rx+self.width+17, self.ry + 38)
+        mouse_movements.perform_click(self)
+        mouse_movements.move_mouse(self, self.rx+ self.width - 63, self.ry + self.height - 30, 4)
+        mouse_movements.perform_click(self)
+        time.sleep(random.uniform(.80, 1.50))
+        mouse_movements.move_mouse(self, self.rx + (self.width/2) + 65, self.ry + 290, 5)
+        mouse_movements.perform_click(self)
+        time.sleep(random.uniform(7.00, 9.00))   
+        mouse_movements.move_mouse(self, self.rx+ (self.width/2) + 65, self.ry + 365, 5) 
+        mouse_movements.perform_click(self)
+    
+    def random_movements(self):
+        pass
